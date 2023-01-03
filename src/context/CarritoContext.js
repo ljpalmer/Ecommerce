7949b88769ffc6970a913c88d8d26ed8067a0eac
ljpalmer <108294729+ljpalmer@users.noreply.github.com>
2023-01-03 15:14:46 -0500

    Fix: CarritoContext

--- src/context/CarritoContext.js
+++ src/context/CarritoContext.js
@@ -1,31 +1,30 @@
-    import { useContext, useState, createContext } from "react";
+import {useContext, useState, createContext} from "react";
 
-    const carritoContext = createContext();
+const CarritoContext = createContext();
+export const useCarritoContext = () => useContext(CarritoContext);
 
-    export const useCarritoContext = useContext(carritoContext);
-
-    export const carritoProvider = (props) => {
+const CarritoProvider = (props) => {
     const [carrito, setCarrito] = useState([]);
-        //Ver Cantidad, agregar productos, eliminar productos
-        //vaciar el carrito, finalizar compra, buscar producto
-        const isInCart = (id) => {
-            //retorna productos pero
-            return carrito.find(producto => producto.id===id);
-        }
+    // Ver Cantidad, agregar productos, eliminar productos vaciar el carrito,
+    // finalizar compra, buscar producto
+    const isInCart = (id) => {
+        //retorna productos pero
+        return carrito.find(producto => producto.id === id);
+    }
 
-        const addItem = (producto, cantidad) =>{
-            if(isInCart(producto.id)){
-                const indice= cart.findIndex(pro => producto.id===producto);
-                const aux = [...carrito] ;
-                aux[indice].cant = cantidad;
-                setCarrito(aux);
-            }else{
-                //No existe
-                const nuevoProducto = {
-                    ...producto,
-                    cant : cantidad
-                    }
-                }
+    const addItem = (producto, cantidad) => {
+        if (isInCart(producto.id)) {
+            const indice = carrito.findIndex(pro => producto.id === producto);
+            const aux = [...carrito];
+            aux[indice].cant = cantidad;
+            setCarrito(aux);
+        } else {
+            //No existe
+            const nuevoProducto = {
+                ...producto,
+                cant: cantidad
             }
         }
     }
+}
+export {CarritoContext, CarritoProvider}
