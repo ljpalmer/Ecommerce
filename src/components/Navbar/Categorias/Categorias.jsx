fc860d2bffede0d6547f37c2bb6902ee023d507d
ljpalmer <108294729+ljpalmer@users.noreply.github.com>
2023-01-13 05:25:12 -0500

    Fix: color de fondo boton para menu navegacion

--- src/components/Navbar/Categorias/Categorias.jsx
+++ src/components/Navbar/Categorias/Categorias.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDarkModeContext } from "../../context/DarkModeContext";
+
 const Categorias = () => {
+  const {darkMode} = useDarkModeContext()
     return (
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-link">
           {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
-          <button className='btn btn-secondary'><Link className="nav-link" to={"/"}>Home</Link></button>
+          <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link className="nav-link" to={"/"}>Home</Link></button>
         </li>
         <li className="nav-link">
-          <button className="btn btn-secondary"><Link className='nav-link' to={"/categoria/Aventura"}>Aventura</Link></button>
+          <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link className='nav-link' to={"/categoria/Aventura"}>Aventura</Link></button>
         </li>
         <li className="nav-link">
-          <button className="btn btn-secondary"><Link className='nav-link' to={"/categoria/Deportes"}>Deportes</Link></button>
+          <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link className='nav-link' to={"/categoria/Deportes"}>Deportes</Link></button>
         </li>
         <li className="nav-link">
-          <button className="btn btn-secondary"><Link className='nav-link' to={"/categoria/Peleas"}>Peleas</Link></button>
+          <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link className='nav-link' to={"/categoria/Peleas"}>Peleas</Link></button>
         </li>
         <li className="nav-link">
-          <button className="btn btn-secondary"><Link className='nav-link' to={"/categoria/Disparos"}>Disparos</Link></button>
+          <button className={`btn ${darkMode ? 'btn-secondary' : 'btn-primary'}`}><Link className='nav-link' to={"/categoria/Disparos"}>Disparos</Link></button>
         </li>
       </ul>
     );
